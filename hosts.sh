5dc68cc3809410ce08b070918dc5579bf9674e7b
Jon Highley <j_highley@yahoo.com>
2021-04-03 18:00:24 -0400

    Delete hosts.sh

--- hosts.sh
+++
@@ -1,6 +0,0 @@
-echo 127.0.0.1	        localhost    >>  /etc/hosts  &&
-echo ::1		            localhost    >>  /etc/hosts  &&
-echo 127.0.1.1	        Mission-Control.localdomain  Mission-Control   >>   /etc/hosts  &&
-
-
-echo "The Script is Finished !"
