2cc75df47d2469d36b48859ddc6eb3979ee86454
Jon Highley <j_highley@yahoo.com>
2021-04-03 17:54:25 -0400

    Update hosts.sh

--- hosts.sh
+++ hosts.sh
@@ -1,3 +1,6 @@
-nano /etc/hosts &
+echo 127.0.0.1	        localhost    >>  /etc/hosts  &&
+echo ::1		            localhost    >>  /etc/hosts  &&
+echo 127.0.1.1	        Mission-Control.localdomain  Mission-Control   >>   /etc/hosts  &&
+
 
 echo "The Script is Finished !"
