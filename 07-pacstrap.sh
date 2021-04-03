a9d29cccfdc9f040c3358a76821f93ba3618022d
Jon Highley <j_highley@yahoo.com>
2021-04-03 18:47:50 -0400

    Update 07-pacstrap.sh

--- 07-pacstrap.sh
+++ 07-pacstrap.sh
@@ -1,7 +1,7 @@
 
 
 
-pacstrap /mnt base base-devel linux-lts linux-firmware sysfsutils usbutils e2fsprogs dosfstools mtools inetutils netctl dhcpcd device-mapper cryptsetup nano less lvm2 dialog wpa_supplicant  &&
+pacstrap /mnt base base-devel linux-lts linux-firmware sysfsutils usbutils e2fsprogs dosfstools mtools inetutils netctl dhcpcd device-mapper cryptsetup nano less lvm2 dialog wpa_supplicant reflector rsync mlocate  &&
 
 echo "This Script is Finished !!"
 
