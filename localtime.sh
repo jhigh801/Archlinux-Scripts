07089e01a8ff718c33f5a9c88c6258de83c8b554
Jon Highley <j_highley@yahoo.com>
2021-04-03 18:01:10 -0400

    Delete localtime.sh

--- localtime.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo rm -rf /etclocaltime &&
-sudo ln -sf /usr/share/zoneinfo/America/Indiana/Indianapolis    >    /etc/localtime
-heclock --systohc &&
-echo "Script is done!" &&
-echo "go to locale.sh"
