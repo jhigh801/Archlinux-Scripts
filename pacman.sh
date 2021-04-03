6b675d890ab9960640bfad435857b855657bd18d
Jon Highley <j_highley@yahoo.com>
2021-04-03 17:49:32 -0400

    Update pacman.sh

--- pacman.sh
+++ pacman.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
 
-sudo pacman-key --init &&
-sudo pacman-key --populate archlinux &&
-sudo pacman -Syyy &&
-sudo pacman -S reflector mlocate  &&
+
+pacman-key --init &&
+pacman-key --populate archlinux &&
+pacman -Syyy &&
+pacman -S reflector mlocate  &&
 echo "Script is Done !!" &&
 echo "Go to Script "Reflector.sh"
