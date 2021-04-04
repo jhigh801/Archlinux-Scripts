c39613b261d2be25c685bbff98a280586aa1a4d1
Jon Highley <j_highley@yahoo.com>
2021-04-03 22:03:42 -0400

    Add files via upload

---
+++ 22-fonts&themes.sh
@@ -0,0 +1,6 @@
+
+
+sudo pacman -S ttf-ubuntu-font-family ttf-dejavu ttf-bitstream-vera ttf-liberation noto-fonts ttf-roboto ttf-opensans opendesktop-fonts cantarell-fonts freetype2   &&
+
+echo "This Script is Finished !!"
+
