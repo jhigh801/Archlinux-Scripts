5743a8ea72f24cefd1043169d515c5d9c804fabd
Jon Highley <j_highley@yahoo.com>
2021-04-03 21:52:23 -0400

    Add files via upload

---
+++ 21-networking.sh
@@ -0,0 +1,6 @@
+ 
+
+ sudo pacman -S --needed  b43-fwcutter broadcom-wl-dkms amd-ucode ipw2100-fw net-tools networkmanager networkmanager-openvpn nm-connection-editor network-manager-applet wget curl firefox filezilla thunderbird wireless_tools nfs-utils nilfs-utils dhclient dnsmasq dmraid dnsutils openvpn openssh openssl samba whois avahi openresolv vsftpd geoip-database   &&
+
+ echo "Thus Script is Finished !!"
+
