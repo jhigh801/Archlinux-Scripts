1df58f88b45b4b9c09a5917e023c845a5bda6d3c
Jon Highley <j_highley@yahoo.com>
2021-08-29 19:03:48 -0400

    Update 03-mount.sh

--- 03-mount.sh
+++ 03-mount.sh
@@ -1,7 +1,7 @@
-
-mount  /dev/sda2   /mnt  &&
-mkdir  /mnt/boot &&
-mount  /dev/sda1   /mnt/boot &&
+mount /dev/sda2  /mnt &&
+mkdir /mnt/{boot,home} &&
+mount /dev/sda1  /mnt/boot &&
+mount /dev/sda3  /mnt/home &&
 
 echo "This Script is Finished !!"
 
