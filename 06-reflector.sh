641c697755db5ecbf954926423d60d233f2eef43
Jon Highley <j_highley@yahoo.com>
2022-03-29 21:48:36 -0400

    updating reflector.sh

--- 06-reflector.sh
+++ 06-reflector.sh
@@ -1,6 +1,6 @@
 
 
-reflector  -c "United States"  -a 15   -p https   --sort rate   --save   /etc/pacman.d/mirrorlist &&
+reflector  --latest 10 --protocol https  --save   /etc/pacman.d/mirrorlist &&
 
 echo "This Script is Finished !!"
 
