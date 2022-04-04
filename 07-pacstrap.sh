25d40c67b87f723472b93cdfa2563a9b8e15db2a
Jon Highley <j_highley@yahoo.com>
2022-04-04 17:59:52 -0400

    Delete 07-pacstrap.sh

--- 07-pacstrap.sh
+++
@@ -1,7 +0,0 @@
-
-
-
-pacstrap /mnt base base-devel linux linux-firmware sysfsutils usbutils e2fsprogs dosfstools mtools networkmanager network-manager-applet nm-connection-editor inetutils iwd netctl dhcpcd device-mapper cryptsetup nano python python-pip python-setuptools less lvm2 dialog wpa_supplicant reflector rsync mlocate  &&
-
-echo "This Script is Finished !!"
-
