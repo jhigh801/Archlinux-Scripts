676da8ce90852b81732c1b164c79e3719cc45dee
Jon Highley <j_highley@yahoo.com>
2022-04-04 18:01:17 -0400

    Delete 21-networking.sh

--- 21-networking.sh
+++
@@ -1,6 +0,0 @@
- 
-
- sudo pacman -S --needed  b43-fwcutter broadcom-wl-dkms amd-ucode ipw2100-fw net-tools networkmanager networkmanager-openvpn nm-connection-editor network-manager-applet wget curl firefox filezilla thunderbird wireless_tools nfs-utils nilfs-utils dhclient dnsmasq dmraid dnsutils openvpn openssh openssl samba whois avahi openresolv vsftpd geoip-database   &&
-
- echo "Thus Script is Finished !!"
-
