d9079654ed438827e0bd2583354675265c572390
Jon Highley <j_highley@yahoo.com>
2022-04-04 18:01:35 -0400

    Delete 22-fonts&themes.sh

--- 22-fonts&themes.sh
+++
@@ -1,7 +0,0 @@
-
-
-sudo pacman -S  --needed noto-fonts  noto-fonts-extra opendesktop-fonts ttf-anonymous-pro ttf-cormorant ttf-croscore ttf-dejavu ttf-hack ttf-fira-mono ttf-inconsolata ttf-joypixels ttf-monofur ttf-monoid ttf-roboto ttf-roboto-mono ttf-ubuntu-font-family ttf-liberation ttf-linux-libertine ttf-linux-libertine-g &&
-sudo pacman -S  --needed git &&
-git clone https://aur.archlinux.org/ttf-ms-win10.git  &&
-echo "This Script is Finished !!"
-
