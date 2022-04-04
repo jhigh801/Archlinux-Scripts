77bfb0a24b81cf6a2613807af04f9491d2e31397
Jon Highley <j_highley@yahoo.com>
2022-04-04 18:00:46 -0400

    Delete 19-general.sh

--- 19-general.sh
+++
@@ -1,4 +0,0 @@
-
-
-sudo pacman -S --needed linux-lts-headers dkms p7zip archiso haveged pacman-contrib pkgfile diffutils jfsutils reiserfsprogs btrfs-progs f2fs-tools logrotate man-db man-pages mdadm perl s-nail texinfo
-which xfsprogs lsscsi sdparm sg3_utils smartmontools fuse2 fuse3 ntfs-3g exfat-utils gvfs gvfs-afc gvfs-goa gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb unrar unzip unace xz xdg-user-dirs xscreensaver grsync ddrescue dd_rescue testdisk hdparm htop rsync hardinfo bash-completion geany geany-plugins lsb-release polkit ufw bleachbit packagekit
