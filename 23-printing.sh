9d6524946b5f3bccb9fc3ad39f26c4d9396dfb9b
Jon Highley <j_highley@yahoo.com>
2022-04-04 18:01:59 -0400

    Delete 23-printing.sh

--- 23-printing.sh
+++
@@ -1,7 +0,0 @@
-
-
-sudo pacman -S --needed system-config-printer foomatic-db foomatic-db-engine gutenprint ghostscript hplip simple-scan cups cups-pdf cups-filter cups-pk-helper gsfont python-pillow python-pyqt5 python-reportlab  &&
-
-echo "This Script is Finished !!"
-
-
