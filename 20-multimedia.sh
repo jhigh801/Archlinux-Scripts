a6c43b373a673fd3695255a56399143b6da9dcbc
Jon Highley <j_highley@yahoo.com>
2022-04-04 18:01:07 -0400

    Delete 20-multimedia.sh

--- 20-multimedia.sh
+++
@@ -1,3 +0,0 @@
-
-
-sudo pacman -S --needed pulseaudio vlc cdrtools gstreamer gst-libav gst-pluigns-base gst-plugins-base-libs gst-plugins-bad gst-plugins-good gst-plugins-ugly gstreamer-vaapi gst-transcoder xvidcore frei0r-plugins cdrdao dvdauthor transcode alsa-utils alsa-plugins alsa-firmware pulseaudio-alsa pulseaudio-equalizer pulseaudio-jack pavucontrol ffmpeg ffmpegthumbnailer libdvdcss gimp inkscape blender imagemagick flac mjpegtools x265 x264 lame sox mencoder faad2 clementine celluloid youtube-dl atomicparsley python-pycryptodome
