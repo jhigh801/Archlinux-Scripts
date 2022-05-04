f8871342aee4d9415a28e897c7dace2c028cf78d
Jon Highley <j_highley@yahoo.com>
2022-05-03 23:07:06 -0400

    Update 10-clean_disk.sh

--- ArchInstaller/10-clean_disk.sh
+++ ArchInstaller/10-clean_disk.sh
@@ -2,6 +2,7 @@
 
 ## --------------------------------- Cleaning-Disk {{{
 setfont ter-v22b
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
 wipefs -a  /dev/sda
 sgdisk -Z  /dev/sda
 ## }}}
