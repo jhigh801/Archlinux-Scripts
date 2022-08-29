d11533b220026193e748b8e5f772caaff6daa327
Jon Highley <j_highley@yahoo.com>
2022-08-28 23:24:02 -0400

    Update 30-mount_Partitions.sh

--- ArchInstaller/30-mount_Partitions.sh
+++ ArchInstaller/30-mount_Partitions.sh
@@ -2,7 +2,8 @@
 
 ## ---------------------- Mounting-Partitions {{{
 mount /dev/sda2   /mnt
-mkdir /mnt/{efi,home}
+mkdir -p /mnt/boot/efi
+mkdir -p /mnt/home
 mount /dev/sda1   /mnt/boot/efi
 mount /dev/sda3   /mnt/home
 ## }}}
