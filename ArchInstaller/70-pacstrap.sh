116d93f84a7faf199011f50108bda9bbcd9ceb0d
Jon Highley <j_highley@yahoo.com>
2023-12-15 10:40:33 -0500

    Update 70-pacstrap.sh

--- ArchInstaller/70-pacstrap.sh
+++ ArchInstaller/70-pacstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## -------------------------- Pacstrap {{{
-pacstrap  /mnt  base base-devel linux-lts linux linux-lts-headers linux-headers linux-firmware sudo nano e2fsprogs reiserfsprogs xfsprogs ntfs-3g dosfstools jfsutils sysfsutils exfatprogs exfat-utils btrfs-progs cifs-utils samba smbclient gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb python python-pip python-setuptools reflector rsync python-lockfile python-pysocks python-brotli python-lxml python-lxml-docs python-genshi python-beautifulsoup4 python-cssselect efibootmgr efivar grub amd-ucode smartmontools
+pacstrap  /mnt  base base-devel linux-lts linux linux-lts-headers linux-headers linux-firmware sudo nano e2fsprogs reiserfsprogs xfsprogs ntfs-3g dosfstools jfsutils sysfsutils exfatprogs exfat-utils btrfs-progs cifs-utils samba smbclient gvfs gvfs-afc gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-smb python python-pip python-setuptools reflector rsync python-lockfile python-pysocks python-brotli python-lxml python-lxml-docs python-genshi python-beautifulsoup4 python-cssselect efibootmgr efivar grub intel-ucode smartmontools
 ## }}}
 
 echo "This Script is now completed!"
