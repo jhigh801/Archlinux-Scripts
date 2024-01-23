8f0714f0f66e63a2ef1faa6d466a1a072eb0f0e8
Jon Highley <jdhighley02@gmail.com>
2024-01-22 20:01:21 -0500

    Update 200-enabling-_services.sh
    written and completed in repo.

--- ArchInstaller/200-enabling-_services.sh
+++ ArchInstaller/200-enabling-_services.sh
@@ -1,1 +1,16 @@
+#!/bin/sh 
 
+systemctl enable NetworkManager.service
+systemctl enable bluetooth.service
+systemctl enable dhcpcd.service
+systemctl enable wpa_supplicant.service
+systemctl enable hddtemp.service
+systemctl enable iwd.service
+systemctl enable tlp.service
+systemctl enable thermald.service
+systemctl enable reflector.service
+systemctl enable rsyncd.service
+systemctl enable systemd-timesyncd.service
+systemctl enable cups.service
+
+echo "This Script is Complete!"
