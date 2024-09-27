f073a479d8dc1b89835539c85f8550e82ca9ed5e
Jon Highley <jdhighley02@gmail.com>
2024-09-26 21:45:48 -0400

    Update 200-enabling-_services.sh
    Commenting out a few services that are unnecessary at this time. 
    Also added the cups-browsed.service.

--- ArchInstaller/200-enabling-_services.sh
+++ ArchInstaller/200-enabling-_services.sh
@@ -2,15 +2,16 @@
 
 systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
-systemctl enable dhcpcd.service
+# systemctl enable dhcpcd.service
 systemctl enable wpa_supplicant.service
 systemctl enable hddtemp.service
 systemctl enable iwd.service
 systemctl enable tlp.service
-systemctl enable thermald.service
+# systemctl enable thermald.service
 systemctl enable reflector.service
 systemctl enable rsyncd.service
+systemctl enable systemd-resolved.service
 systemctl enable systemd-timesyncd.service
 systemctl enable cups.service
-
+systemctl enable cups-browsed.service
 echo "This Script is Complete!"
