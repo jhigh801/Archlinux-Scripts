5bd63fcb394951193ed069bf02516e5bbfe53740
Jon Highley <jdhighley02@gmail.com>
2024-10-27 00:15:44 -0400

    Update Graphics.sh

--- Graphics/Graphics.sh
+++ Graphics/Graphics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## ------------------------ Graphics Apps {{{
-sudo pacman -Syy ; sudo pacman -S --noconfirm --needed inkscape gimp blender gpick nomacs viewnior
+sudo pacman -Syy ; sudo pacman -S --noconfirm --needed inkscape gimp blender gpick viewnior
 ## }}}
 
 echo "This Script is now Complete!"
