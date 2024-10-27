34c7485e5a6f0f36636e732bcc9052e697e42d1c
Jon Highley <jdhighley02@gmail.com>
2024-10-26 23:43:54 -0400

    Update 200-enabling-_services.sh

--- ArchInstaller/200-enabling-_services.sh
+++ ArchInstaller/200-enabling-_services.sh
@@ -2,14 +2,14 @@
 
 systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
-# systemctl enable dhcpcd.service
+systemctl enable dhcpcd.service
 systemctl enable wpa_supplicant.service
 systemctl enable hddtemp.service
 systemctl enable iwd.service
 systemctl enable tlp.service
-# systemctl enable thermald.service
 systemctl enable reflector.service
 systemctl enable rsyncd.service
+systemctl enable avahi-daemon.service
 systemctl enable systemd-resolved.service
 systemctl enable systemd-timesyncd.service
 systemctl enable cups.service
