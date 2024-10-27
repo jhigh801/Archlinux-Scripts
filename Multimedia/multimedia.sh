e3bf0d43ecbe548234d05f3abc72e70f7ac1cc17
Jon Highley <jdhighley02@gmail.com>
2024-10-27 00:16:30 -0400

    Update multimedia.sh

--- Multimedia/multimedia.sh
+++ Multimedia/multimedia.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## ------------------------ Multimedia-Apps {{{
-sudo pacman -Syy ; sudo pacman -S --noconfirm --needed vlc mpv celluloid rhythmbox audacious audacious-plugins clementine yt-dlp atomicparsley python-pycryptodomex aria2 ffmpeg python-mutagen python-websockets rtmpdump python-certifi
+sudo pacman -Syy ; sudo pacman -S --noconfirm --needed vlc mpv celluloid rhythmbox audacious audacious-plugins yt-dlp atomicparsley python-pycryptodomex aria2 ffmpeg python-mutagen python-websockets rtmpdump python-certifi
 ## }}}
 
 echo "This Script is now complete!"
