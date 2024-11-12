2d396409d0ea1322c48b771f30bce6a0be69c318
Jon Highley <jdhighley02@gmail.com>
2024-11-12 18:53:19 -0500

    Update 10-network.sh
    added some changes to 10-network.sh to make it easier for me when I install Archlinux at a later date.

--- Network/10-network.sh
+++ Network/10-network.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## ------------------------ Network Apps {{{
-sudo pacman -Syy ; sudo pacman -S --noconfirm --needed networkmanager network-manager-applet nm-connection-editor wpa_supplicant iwd firefox firefox-i18n-en-us ttf-fira-mono ttf-fira-sans ttf-roboto ttf-monoid ttf-monofur ttf-inconsolata firefox-adblock-plus geoip-database geoip-database-extra gd geoipupdate psutils
+sudo pacman -Syy ; sudo pacman -S --noconfirm --needed networkmanager network-manager-applet nm-connection-editor wpa_supplicant iwd bluez bluez-libs bluez-utils bluez-tools dhcpcd firefox firefox-i18n-en-us ttf-fira-mono ttf-fira-sans ttf-roboto ttf-monoid ttf-monofur ttf-inconsolata firefox-adblock-plus geoip-database geoip-database-extra gd geoipupdate psutils
 ## }}}
 ## ------------------------ enabling Network Apps {{{
 sudo systemctl enable NetworkManager.service
