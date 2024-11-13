7499e6dcfa9c60b7f543d573287a7430a4e57587
Jon Highley <jdhighley02@gmail.com>
2024-11-12 19:00:40 -0500

    Update 70-Xfce4.sh

--- Desktop_Environments/70-Xfce4.sh
+++ Desktop_Environments/70-Xfce4.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ## ------------------------ Xfce4-Desktop {{{
-sudo pacman -Syy ;  sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies xfconf xfwm4 xfwm4-themes thunar ffmpeg ffmpegthumbnailer catfish engrampa atril pluma mlocate gnome-disk-utility gparted gpart lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings accountsservice
+sudo pacman -Syy ;  sudo pacman -S --noconfirm --needed xfce4 xfce4-goodies xfconf xfwm4 xfwm4-themes thunar thunar-volman thunar-archive-plugin thunar-media-tags-plugin ffmpeg ffmpegthumbnailer catfish engrampa atril pluma mlocate gnome-disk-utility gparted gpart lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings accountsservice
 ## }}}
 ## ------------------------ lightdm {{{
 sudo systemctl enable lightdm.service
