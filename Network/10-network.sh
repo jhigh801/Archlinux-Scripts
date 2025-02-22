90965c07aa8fb2c93fb133e843e2e791071fb8a7
Jon Highley <jdhighley02@gmail.com>
2025-02-21 20:05:45 -0500

    Update 10-network.sh
    added  and removed some entries in the script. just some needed housekeeping.

--- Network/10-network.sh
+++ Network/10-network.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ## ------------------------ Network Apps {{{
-sudo pacman -Syy ; sudo pacman -S --noconfirm --needed networkmanager network-manager-applet nm-connection-editor wpa_supplicant iwd bluez bluez-libs bluez-utils bluez-tools dhcpcd firefox firefox-i18n-en-us ttf-fira-mono ttf-fira-sans ttf-roboto ttf-monoid ttf-monofur ttf-inconsolata firefox-adblock-plus geoip-database geoip-database-extra gd geoipupdate psutils
-## }}}
+sudo pacman -Syy ; sudo pacman -S --noconfirm --needed networkmanager network-manager-applet nm-connection-editor wpa_supplicant iwd bluez bluez-libs bluez-utils bluez-tools dhcpcd ttf-fira-mono ttf-fira-sans ttf-roboto ttf-monoid ttf-monofur ttf-inconsolata geoclue geoip-database geoip-database-extra gd geoipupdate psutils
+## }}
 ## ------------------------ enabling Network Apps {{{
 sudo systemctl enable NetworkManager.service
 sudo systemctl enable iwd.service
