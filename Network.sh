966ff45490633fa7cee0746aca53ae73babcb7f2
Jon Highley <jdhighley02@gmail.com>
2025-05-24 22:26:50 -0400

    Update Network.sh
    Correcting typographical errors.

--- Network.sh
+++ Network.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pacman -S --needed --nocnfirm networkmanager network-manager-applet nm-connection-editor bluez bluez-libs bluez-tools bluez-utils dhcpcd openresolv dnsutils dnsmasq dhclient iw iwd wireless_tools wireless-regdb wpa_supplicant tlp tk tcl gd ed geoip geoip-database geoip-database-extra reflector rsync geoipupdate geoclue hddtemp smartmontools &&
+pacman -S --needed --noconfirm networkmanager network-manager-applet nm-connection-editor bluez bluez-libs bluez-tools bluez-utils dhcpcd openresolv dnsutils dnsmasq dhclient iw iwd wireless_tools wireless-regdb wpa_supplicant tlp tk tcl gd ed geoip geoip-database geoip-database-extra reflector rsync geoipupdate geoclue hddtemp smartmontools &&
 
 systemctl enable NetworkManager.service &&
 systemctl enable bluetooth.service &&
@@ -10,7 +10,7 @@ systemctl enable tlp.service &&
 systemctl enable reflector.service &&
 systemctl enable rsyncd.service &&
 systemctl enable hddtemp.service &&
-systemctl enable avahi.service &&
+systemctl enable avahi-daemon.service &&
 systemctl enable fancontrol.service &&
 systemctl enable systemd-resolved &&
 systemctl enable systemd-timesyncd.service &&
