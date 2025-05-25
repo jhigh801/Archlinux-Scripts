bb167ebdb1c84d413e89fc625f6c9d7e894c12cf
Jon Highley <jdhighley02@gmail.com>
2025-05-24 22:28:55 -0400

    Update Printing.sh
    Correcting typographical errors.

--- Printing.sh
+++ Printing.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-pacman -S --needed --noconfirm cups cups-pdf cups-pk-helper cups-filters cups-browsed python-pycups libcups libcupsfilters system-config-printer hplip python-pillow python-reportlab gutenprint ghostscript foomatic-db foomatic-db-engine foomatic-db-gutenprint foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-ppds &&
+pacman -S --needed --noconfirm cups cups-pdf cups-pk-helper cups-filters cups-browsed python-pycups libcups libcupsfilters system-config-printer hplip python-pillow python-reportlab gutenprint ghostscript foomatic-db foomatic-db-engine foomatic-db-gutenprint-ppds foomatic-db-nonfree foomatic-db-nonfree-ppds foomatic-db-ppds &&
 
 systemctl enable cups.service &&
 systemctl enable cups-browsed.service &&
