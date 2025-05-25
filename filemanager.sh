64667e2b1982ed76dfe049839006c131c101b94c
Jon Highley <jdhighley02@gmail.com>
2025-05-24 22:24:27 -0400

    Create filemanager.sh
    created new bash script to install thunar and it's plugins.

---
+++ filemanager.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+pacman -S --needed --noconfirm thunar thunar-archive-plugin thunar-media-tags-plugin thunar-shares-plugin thunar-vcs-plugin thunar-volman gvfs gvfs-goa gvfs-google gvfs-gphoto2 gvfs-mtp gvfs-nfs gvfs-onedrive gvfs-smb &&
+echo "This script is now complete!"
