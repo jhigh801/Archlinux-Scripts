393c10ec587f1cd107ae34e6875bcd2aef266124
Jon Highley <j_highley@yahoo.com>
2025-06-13 15:36:30 -0400

    uploading lain-freedesktop script

---
+++ creating-lain-freedesktop.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone https://aur.archlinux.org/lain-git &&
+git clone https://aur.archlinux.org/awesome-freedesktop-git &&
+
+-- cd lain-git/ &&
+-- makepkg -si PKGBUILD
+
+-- cd awesome-freedesktop-git/ &&
+-- makepkg -si PKGBUILD
