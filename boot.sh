c51ddb41d319e50d36b3ec0c483fb568052fb544
Jon Highley <jdhighley02@gmail.com>
2025-07-04 00:37:36 -0400

    Update boot.sh
    correcting a typo.

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/env bash
 
-pacman -S --needed --noconfirm grub efibootmgr efivar dosfstools jfsutils sysfsutils erofs-utils exfat-utils exfatprogs	nfs-utils nilfs-utils intel-ucode amd-ucode f2fs-tool elfutils &&
+pacman -S --needed --noconfirm grub efibootmgr efivar dosfstools jfsutils sysfsutils erofs-utils exfat-utils exfatprogs	nfs-utils nilfs-utils intel-ucode amd-ucode f2fs-tools elfutils &&
 
 echo "This Script is now finished!"
