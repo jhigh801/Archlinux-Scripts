f2aaa129a2ef6a8a14740f19271a2c14245f1f28
Jon Highley <j_highley@yahoo.com>
2025-12-21 23:42:40 +0000

    adding git.sh
    adding git.sh

---
+++ git.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo pacman -S --needed --noconfirm git curl expat grep openssl pcre2 perl perl-error perl-mailtools shadow zlib-ng less libsecret openssh man-db man-pages plocate logrotate perl-authen-sasl perl-cgi perl-io-socket-ssl perl-libwww perl-term-readkey subversion tk tcl pcre2 rust devtools gdb lldb bat asciidoc 
+
