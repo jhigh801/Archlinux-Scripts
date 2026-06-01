6ac5c226fa22f7db3b7b9894352ae2c4f1792699
Jon Highley <jdhighley02@gmail.com>
2026-06-01 02:03:40 -0400

    Delete ArchInstaller/160-useradd.sh

--- ArchInstaller/160-useradd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-## ---------------------------- Useradd {{{
-useradd -m -g users -G sys,log,network,scanner,power,rfkill,users,jdhighley,video,storage,optical,lp,audio,wheel,adm  -s  /bin/bash  jon
-## }}}
-
-echo "This Script is now complete!"
-
