db05c7aa91e0785612494dafd0d7df193fcc8e61
Jon Highley <jdhighley02@gmail.com>
2026-06-01 02:04:44 -0400

    Delete ArchInstaller/190-grub.sh

--- ArchInstaller/190-grub.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-## ----------------------Grub {{{
-grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB --recheck  /dev/sda
-grub-mkconfig -o  /boot/grub/grub.cfg
-## }}}
-
-echo "This Script is now complete!"
