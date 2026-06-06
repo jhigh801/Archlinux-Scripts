d46bb2ecfd09f0296f2f10d34650acb85f9e563c
Jon Highley <j_highley@yahoo.com>
2026-06-06 21:09:18 +0000

    uploading ranger-06-06-2026.sh to repo.

---
+++ ranger-06-06-2026.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm atool zip unzip cpio 7zip p7zip lhasa unace unrar bzip2 xz tar xarchiver arj unarj lrzip lz4 lzip lzop xdg-utils zstd highlight mediainfo transmission-cli ueberzug ffmpegthumbnailer imagemagick libcaca lynx elinks w3m odt2txt perl-image-exiftool perl-io-compress-brotli perl-archive-zip poppler python-chardet python-pillow sudo python-build python-installer python-setuptools python-wheel python-pytest python
